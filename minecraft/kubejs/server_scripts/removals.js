0fa11a0f3f2df109ec32d279ada262e83c7f36aa
Deepa <52133154+Deepacat@users.noreply.github.com>
2024-11-19 02:59:51 -0600

    more removals and formatting

--- minecraft/kubejs/server_scripts/removals.js
+++ minecraft/kubejs/server_scripts/removals.js
@@ -1,6 +1,10 @@
 //priority: 999
 ServerEvents.recipes(e => {
     // The beginning of suffering. (Nukes most gt recipes except ones my ass is not making a builder for)
+    // e.remove({ type: "gtceu:extruder" })
+    // e.remove({ type: "gtceu:extruder", output: /.*rod/, not: { output: /.*long.*/ } })
+    // e.remove({ mod: "pneumaticcraft" })
+
     e.remove({
         mod: "gtceu",
         not: [
@@ -10,25 +14,27 @@ ServerEvents.recipes(e => {
             { type: "gtceu:forge_hammer" },
             { type: "gtceu:packer" },
             { type: "gtceu:alloy_smelter" },
-            { type: "gtceu:compressor" },
-            { type: "gtceu:extractor" },
-            { type: "gtceu:compressor" },
+            { id: /gtceu:compressor\/compress.*/ },
+            { id: /gtceu:compressor\/.*plant.*/ },
+            { id: /gtceu:extractor\/extract.*/ },
             { type: "gtceu:wiremill" },
             { type: "gtceu:lathe" },
-            { type: "gtceu:compressor" }
         ]
     })
 
-    e.remove({ type: "gtceu:extruder", output: /.*rod/, not: { output: /.*long.*/ } })
-
-    e.remove({ mod: "pneumaticcraft" })
-
-    e.remove([
-        { type: "ad_astra:compressing" },
-        { type: "ad_astra:alloying" },
-        { type: "createaddition:rolling" },
-        { type: "create:pressing" }
-    ])
+    let removeTypes = [
+        { type: "pneumaticcraft:explosion_crafting" }, { type: "pneumaticcraft:etching_tank" }, { type: "pneumaticcraft:assembly" },
+        { type: "pneumaticcraft:amadron_trade" }, { type: "pneumaticcraft:memory_essence" }, { type: "pneumaticcraft:yeast_crafting" },
+        { type: "pneumaticcraft:uv_light_box" }, { type: "pneumaticcraft:thermo_plant" }, { type: "pneumaticcraft:spawner_extraction" },
+        { type: "pneumaticcraft:refinery" }, { type: "pneumaticcraft:pressure_chamber" }, { type: "pneumaticcraft:plastic_solidifying" },
+        { type: "pneumaticcraft:heat_frame_cooling" }, { type: "pneumaticcraft:fluid_mixer" }, { type: "ad_astra:compressing" },
+        { type: "ad_astra:alloying" }, { type: "ad_astra:refining" }, { type: "alexsmobs:capsid" },
+        { type: "alexscaves:nuclear_furnace" }, { type: "alexscaves:spelunkery_table" }, { type: "createaddition:rolling" },
+        { type: "create:pressing" }, { type: /create\/crushing.*/}
+    ]
+    removeTypes.forEach(recipeType => {
+        e.remove(recipeType)
+    })
 
     // Remove recipes for all the things added to nukelist
     global.nukeList.forEach((item) => {
