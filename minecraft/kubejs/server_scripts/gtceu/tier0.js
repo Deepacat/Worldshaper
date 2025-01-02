24a567f1956d3de26c0080a63e69252179c7bebb
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-01-02 04:51:14 -0600

    placeholder tool recipse

--- minecraft/kubejs/server_scripts/gtceu/tier0.js
+++ minecraft/kubejs/server_scripts/gtceu/tier0.js
@@ -31,10 +31,43 @@ ServerEvents.recipes(e => {
             .duration(r.duration)
             .EUt(EUt)
     })
-    
+
     // ULV Machine Recipes
     e.recipes.gtceu.coal_burner_recipe_type('kubejs:gtceu/test/coal_tag_burning')
         .itemInputs('#minecraft:coals')
         .EUt(-8)
         .duration(200)
+
+    // GT Tools
+    e.shaped(Item.of('gtceu:iron_hammer', 1, { MaxDamage: 1024 }), [
+        ' BI',
+        ' SB',
+        'S  '
+    ], {
+        'S': 'minecraft:stick',
+        'I': '#forge:ingots/iron',
+        'B': '#forge:storage_blocks/iron'
+    })
+    e.shaped(Item.of('gtceu:steel_hammer', 1, { Unbreakable: 1 }), [
+        ' BI',
+        ' SB',
+        'S  '
+    ], {
+        'S': 'minecraft:stick',
+        'I': '#forge:ingots/steel',
+        'B': '#forge:storage_blocks/steel'
+    })
+    function hammerIngotToPlate(ingot, plate) {
+        e.shaped(Item.of(plate), [
+            'H  ',
+            'I  ',
+            'I  '
+        ], {
+            'H': '#forge:tools/hammers',
+            'I': ingot
+        }).id(`kubejs:hammering/${plate.split(':')[1]}`)
+    }
+
+    hammerIngotToPlate('minecraft:iron_ingot', 'gtceu:iron_plate')
+    hammerIngotToPlate('minecraft:gold_ingot', 'gtceu:gold_plate')
 })
