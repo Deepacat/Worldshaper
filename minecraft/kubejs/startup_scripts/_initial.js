47d54cf2be89895659d2891e6b14bb76bae71292
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-01-11 21:22:17 -0600

    moving files, configs, unhide comps

--- minecraft/kubejs/startup_scripts/_initial.js
+++ minecraft/kubejs/startup_scripts/_initial.js
@@ -45,7 +45,7 @@ global.maxVoltage = 4
 // list of regex's to nuke, only does gregtech items
 let regexToNuke = [
     // comps
-    // 'board', 'chip', 'wafer', 'boule', 'transistor', 'resistor', 'capacitor', 'diode', 'inductor', 'soc', 'cpu',
+    'board', 'chip', 'wafer', 'boule', 'transistor', 'resistor', 'capacitor', 'diode', 'inductor', 'soc', 'cpu',
     // materials (WOW THIS IS AWFUL)
     'hastelloy_c_276', 'titanium_tungsten_carbide', 'titanium_carbide', 'stellite_100', 'hastelloy_x', 'maraging_steel_300', 'incoloy_ma_956', 'watertight_steel', 'zeron_100', 'molybdenum_disilicide', 'hsla_steel', 'tantalum_carbide', 'blue_alloy', 'hsss', 'hsse', 'hssg', 'red_alloy', 'blue_steel', 'red_steel', 'borosilicate_glass', 'potin', 'ruthenium_trinium_americium_neutronate', 'enriched_naquadah_trinium_europium_duranide', 'uranium_rhodium_dinaquadide', 'indium_tin_barium_titanium_cuprate', 'samarium_iron_arsenic_oxide', 'uranium_triplatinum', 'mercury_barium_calcium_cuprate', 'magnesium_dioboride', 'manganese_phosphide', 'tungsten_carbide', 'nickel_zinc_ferrite', 'indium_gallium_phosphide', 'gallium_arsenide', 'yttrium_barium_cuprate', 'vanadium_gallium', 'magnalium', 'niobium_nitride', 'niobium_titanium', 'bismith_bronze', 'black_bronze', 'rose_gold', 'sterling_silver', 'samarium', 'tantalum', 'yttrium', 'plutonium_241', 'niobium', 'neodymium', 'molybdenum', 'manganese', 'bismuth', 'beryllium', 'cobalt', 'damascus_steel', 'black_steel', 'opal', 'red_garnet', 'yellow_garnet', 'olivine', 'sodalite', 'lazurite', 'ruby', 'sapphire', 'blue_topaz', 'emerald', 'green_sapphire', 'topaz', 'amethyst', 'echo_shard', 'tungsten_steel', 'tin_alloy', 'magnesium_diboride', 'polycaprolactam', 'soldering_alloy', 'ultimet'
 ]
