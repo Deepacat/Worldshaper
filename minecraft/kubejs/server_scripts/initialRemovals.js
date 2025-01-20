9111617fc1e6e1e6dba61b0528bcead282f9c917
Divine Ray <29178731+divine-ray@users.noreply.github.com>
2025-01-20 04:10:43 +0100

    edit InitialRemovals.js: grouped removal per mod

--- minecraft/kubejs/server_scripts/initialRemovals.js
+++ minecraft/kubejs/server_scripts/initialRemovals.js
@@ -13,14 +13,33 @@ ServerEvents.recipes(e => {
 
     // large recipe groups for removing
     let removeTypes = [
-        { type: "pneumaticcraft:explosion_crafting" }, { type: "pneumaticcraft:etching_tank" }, { type: "pneumaticcraft:assembly" },
-        { type: "pneumaticcraft:amadron_trade" }, { type: "pneumaticcraft:memory_essence" }, { type: "pneumaticcraft:yeast_crafting" },
-        { type: "pneumaticcraft:uv_light_box" }, { type: "pneumaticcraft:thermo_plant" }, { type: "pneumaticcraft:spawner_extraction" },
-        { type: "pneumaticcraft:refinery" }, { type: "pneumaticcraft:pressure_chamber" }, { type: "pneumaticcraft:plastic_solidifying" },
-        { type: "pneumaticcraft:heat_frame_cooling" }, { type: "pneumaticcraft:fluid_mixer" }, { type: "ad_astra:compressing" },
-        { type: "ad_astra:alloying" }, { type: "ad_astra:refining" }, { type: "alexsmobs:capsid" },
-        { type: "alexscaves:nuclear_furnace" }, { type: "alexscaves:spelunkery_table" }, { type: "createaddition:rolling" },
-        { type: "create:pressing" }, { output: "create:experience_nugget", type: "create:crushing" }
+        //Pneumaticcraft
+        { type: "pneumaticcraft:explosion_crafting" },
+        { type: "pneumaticcraft:etching_tank" },
+        { type: "pneumaticcraft:assembly" },
+        { type: "pneumaticcraft:amadron_trade" },
+        { type: "pneumaticcraft:memory_essence" },
+        { type: "pneumaticcraft:yeast_crafting" },
+        { type: "pneumaticcraft:uv_light_box" },
+        { type: "pneumaticcraft:thermo_plant" },
+        { type: "pneumaticcraft:spawner_extraction" },
+        { type: "pneumaticcraft:refinery" },
+        { type: "pneumaticcraft:pressure_chamber" },
+        { type: "pneumaticcraft:plastic_solidifying" },
+        { type: "pneumaticcraft:heat_frame_cooling" },
+        { type: "pneumaticcraft:fluid_mixer" },
+        //AdAstra
+        { type: "ad_astra:compressing" },
+        { type: "ad_astra:alloying" },
+        { type: "ad_astra:refining" },
+        //AlexMobs
+        { type: "alexsmobs:capsid" },
+        { type: "alexscaves:nuclear_furnace" },
+        { type: "alexscaves:spelunkery_table" },
+        //Create + Addons
+        { type: "createaddition:rolling" },
+        { type: "create:pressing" }, 
+        { output: "create:experience_nugget", type: "create:crushing" }
     ]
 
     removeTypes.forEach(recipeType => {
