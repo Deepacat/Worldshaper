51b9a572dea211bcc6e8f2fbf157ac51d9fabdd4
Divine Ray <29178731+divine-ray@users.noreply.github.com>
2025-02-01 06:23:52 +0100

    tag custom coals as general coal

--- minecraft/kubejs/server_scripts/itemTags.js
+++ minecraft/kubejs/server_scripts/itemTags.js
@@ -1,3 +1,6 @@
-ServerEvents.tags('item', e => {
+ServerEvents.tags('item', event => {
 
+    event.add('minecraft:coals', 'gtceu:lignite_gem')
+    event.add('minecraft:coals', 'gtceu:anthracite_gem')
+    event.add('minecraft:coals', 'gtceu:bituminous_coal_gem')
 })
