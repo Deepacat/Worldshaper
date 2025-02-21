0fb44d7d1774199996dd13379ee6e8d437a35240
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-02-20 20:59:06 -0600

    redid oregen
    now uses custom veins with biome modifier instead of replacing vanilla veins

--- minecraft/kubejs/startup_scripts/worldgen.js
+++ minecraft/kubejs/startup_scripts/worldgen.js
@@ -1,7 +1,8 @@
 WorldgenEvents.remove(event => {
-  // print all features for a given biome filter
-  // event.printFeatures('', 'minecraft:plains')
-  event.removeFeatureById('fluid_springs', 'gtceu:raw_oil_sprout')
-  event.removeFeatureById('lakes', 'pneumaticcraft:oil_lake_surface')
-  event.removeFeatureById('lakes', 'pneumaticcraft:oil_lake_underground')
-})  
+	// print all features for a given biome filter
+	// event.printFeatures('', 'minecraft:plains')
+	// event.printFeatures('underground_ores', '#minecraft:is_overworld')
+	event.removeFeatureById('fluid_springs', 'gtceu:raw_oil_sprout')
+	event.removeFeatureById('lakes', 'pneumaticcraft:oil_lake_surface')
+	event.removeFeatureById('lakes', 'pneumaticcraft:oil_lake_underground')
+})
