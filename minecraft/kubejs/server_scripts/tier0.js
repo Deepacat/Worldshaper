ab4ff4356148eb3c97658590b1556a7dd1c5ce7b
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-02-26 01:30:01 -0600

    move tier0 file, commented things out that gt fork do

---
+++ minecraft/kubejs/server_scripts/tier0.js
@@ -0,0 +1,37 @@
+//priority: 1
+ServerEvents.recipes(e => {
+    // ULV Machine Recipes
+    e.recipes.gtceu.coal_burner_recipe_type('kubejs:gtceu/test/coal_tag_burning')
+        .itemInputs('#minecraft:coals')
+        .EUt(-8)
+        .duration(200)
+
+
+    e.recipes.custommachinery.custom_craft('custommachinery:machine_workbench', 'minecraft:dirt')
+        .jei()
+        .requireItem(Item.of('minecraft:iron_ingot', 4), 'in0')
+        .requireItem(Item.of('gtceu:iron_plate', 2), 'in1')
+})
+
+// Plate recipes
+// plateRecipe(GTMaterials.Iron, '', 3)
+// plateRecipe(GTMaterials.Gold, '', 2)
+// plateRecipe(GTMaterials.Copper, '', 3)
+// plateRecipe(GTMaterials.Tin, '', 3)
+// plateRecipe(GTMaterials.Lead, '', 3)
+// plateRecipe('#forge:gems/diamond', GTMaterials.Diamond, 3)
+
+// Rod recipes
+// rodRecipe(GTMaterials.Iron, 2)
+// rodRecipe(GTMaterials.Tin, 2)
+// rodRecipe(GTMaterials.Copper, 2)
+
+// Gear recipes
+// gearRecipe(GTMaterials.Iron, 2)
+// gearRecipe(GTMaterials.Bronze, 2)
+
+// Screw recipes
+// screwRecipe(GTMaterials.Iron, 2)
+
+// Bolt recipes
+// boltRecipe(GTMaterials.Iron, 2)
