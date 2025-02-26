ae3bbf19be75399f90c7255ee51197f0cc9dbce6
Deepa <52133154+Deepacat@users.noreply.github.com>
2025-02-26 01:30:47 -0600

    comment

--- minecraft/kubejs/startup_scripts/gtceu/worldGen.js
+++ minecraft/kubejs/startup_scripts/gtceu/worldGen.js
@@ -1,4 +1,5 @@
 GTCEuStartupEvents.registry('gtceu:world_gen_layer', event => {
+    // Make a world layer for ores that spawn in the regular overworld and the flat overworld
     event.create('owstones')
         .targets('#minecraft:stone_ore_replaceables')
         .dimensions('minecraft:overworld', 'worldshaper:wsoverworld')
