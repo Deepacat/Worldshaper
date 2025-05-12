6cf37d9b5ff4ec7d37daa6c12d65d469c3b1926f
Divine Ray <29178731+divine-ray@users.noreply.github.com>
2025-05-12 23:59:43 +0200

    dementia marker

--- minecraft/kubejs/server_scripts/itemTags.js
+++ minecraft/kubejs/server_scripts/itemTags.js
@@ -3,4 +3,7 @@ ServerEvents.tags('item', event => {
     event.add('minecraft:coals', 'gtceu:lignite_gem')
     event.add('minecraft:coals', 'gtceu:anthracite_gem')
     event.add('minecraft:coals', 'gtceu:bituminous_coal_gem')
+
+    // #todo: dynamic function to tag appropriate minerals with ex. "primary iron mineral" for minerals that are majorly iron, etc
+     
 })
