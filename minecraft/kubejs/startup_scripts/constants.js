7b01ff6e8786fae76ba8120df3651b29c69e67a5
Divine Ray <29178731+divine-ray@users.noreply.github.com>
2025-05-15 21:14:20 +0200

    add: constants for defining colours (oops i forgot)

---
+++ minecraft/kubejs/startup_scripts/constants.js
@@ -0,0 +1,31 @@
+//priority: 10
+//Script must have high priority, otherwise colors may load too late.
+
+//Minecraft
+const copper = 0xc15a36;
+const iron = 0xd8d8d8;
+const gold = 0xfdf55f;
+//Immersive Engineering
+const tin = 0x465d6b;
+const silver = 0x83aab2;
+const lead = 0x453d5e;
+const nickel = 0xd8b87d;
+const steel = 0x43464b;
+const aluminum = 0xa2a8ab;
+const electrum = 0xe3c739;
+const constantan = 0xf5a280;
+//Mekanism
+const osmium = 0x67869f;
+const bronze = 0xd07c1b;
+const uranium = 0x37a119;
+//Create
+const zinc = 0x96b685;
+const brass = 0xe9ba54;
+//Thermal Series
+const enderium = 0x1c7aa0;
+const lumium = 0xfffd7e;
+const invar = 0x87928c;
+const signalum = 0xe4340f;
+//Custom made by FooterMan
+const tungsten = 0xb5ff9f;
+const signar = 0xc37e48;
